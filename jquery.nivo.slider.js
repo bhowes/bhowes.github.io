f3d58dc7879d2f8f3c51e0aaaf3f8cbebc6b487f
bhowes <brittany.howes@gmail.com>
2014-01-02 11:50:12 -0500

    Add hover state to portfolio link

--- jquery.nivo.slider.js
+++ jquery.nivo.slider.js
@@ -635,12 +635,12 @@
     
     //Default settings
     $.fn.nivoSlider.defaults = {
-        effect: 'random',
-        slices: 15,
+        effect: 'fold',
+        slices: 5,
         boxCols: 8,
         boxRows: 4,
-        animSpeed: 500,
-        pauseTime: 3000,
+        animSpeed: 1000,
+        pauseTime: 4000,
         startSlide: 0,
         directionNav: true,
         controlNav: true,
