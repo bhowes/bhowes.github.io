049cdfd29de6e6556a8d2cb6effd747f06c0a55e
bhowes <brittany.howes@gmail.com>
2014-01-23 13:11:06 -0500

    more PHP...

--- contactengine.php
+++ contactengine.php
@@ -2,18 +2,20 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $from = 'From: BrittanyHowes'; 
+    $from = 'From: Brittany Howes'; 
     $to = 'brittany.howes@gmail.com'; 
-    $subject = 'ContactFromBHowes';
-
-   	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
-
-
-if ($_POST['submit']) {
-    if (mail ($to, $subject, $body, $from)) { 
-        echo '<p>Your message has been sent!</p>';
-    } else { 
-        echo '<p>Something went wrong, go back and try again!</p>'; 
+    $subject = 'Hello';
+    $human = $_POST['human'];
+			
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+				
+    if ($_POST['submit'] && $human == '4') {				 
+        if (mail ($to, $subject, $body, $from)) { 
+	    echo '<p>Your message has been sent!</p>';
+	} else { 
+	    echo '<p>Something went wrong, go back and try again!</p>'; 
+	} 
+    } else if ($_POST['submit'] && $human != '4') {
+	echo '<p>You answered the anti-spam question incorrectly!</p>';
     }
-}
 ?>
