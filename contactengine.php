dff592b5d4d0bc2bcd1ea6284eaafebc9aba4a17
bhowes <brittany.howes@gmail.com>
2014-02-25 11:28:08 -0500

    updated PHP response on contact form

--- contactengine.php
+++ contactengine.php
@@ -1,61 +1,77 @@
-<?php
-/* Set e-mail recipient */
-$myemail = "brittany.howes@gmail.com";
-
-/* Check all form inputs using check_input function */
-$name = check_input($_POST['name']);
-$email = check_input($_POST['email']);
-$message = check_input($_POST['message']);
-
-/* If e-mail is not valid show error message */
-if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
-{
-show_error("E-mail address not valid");
-}
-/* Let's prepare the message for the e-mail */
-$message = "
-
-Name: $name
-E-mail: $email
-
-Message:
-$message
-
-";
-
-/* Send the message using mail() function */
-mail($myemail, $subject, $message);
-
-/* Redirect visitor to the thank you page */
-header('Location: thanks.html');
-exit();
-
-/* Functions we used */
-function check_input($data, $problem='')
-{
-$data = trim($data);
-$data = stripslashes($data);
-$data = htmlspecialchars($data);
-if ($problem && strlen($data) == 0)
-{
-show_error($problem);
-}
-return $data;
-}
-
-function show_error($myError)
-{
-?>
+<!doctype html>
 <html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="keywords" content="">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<meta name="viewport" content="width=device-width" >
+		<title>Brittany Howes Portfolio</title>
+		<link rel="stylesheet" href="style.css">
+		<link href='http://fonts.googleapis.com/css?family=Karla:400,700' rel='stylesheet' type='text/css'>
+
+
+
+
+	</head>
+
 <body>
+		<div id="wrap">
+			<div id="main">
+				<header>
 
-<p>Please correct the following error:</p>
-<strong><?php echo $myError; ?></strong>
-<p>Hit the back button and try again</p>
+					<div id="navigation">
+							<div class="centeredlogo">
+								<div class="logo">
+									<a href=index.html></a>
+								</div>
+							</div>
 
-</body>
-</html>
-<?php
-exit();
-}
+							<div>
+								<ul class="navbuttons">
+									<li>
+										<a class="activebutton" href="#">Work</a>
+									</li>
+									<li>
+										<a class="button" href= "about.html">About</a>
+									</li>
+									<li>
+										<a class="button" href="contact.html">Contact</a>
+									</li>
+								</ul>
+							</div>
+					</div>
+				</header>
+
+
+
+<?php 
+$ToEmail = 'brittany.howes@gmail.com'; 
+$EmailSubject = 'Bhowes contact form'; 
+$mailheader = "From: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+$MESSAGE_BODY = "Name: ".$_POST["name"].""; 
+$MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
+$MESSAGE_BODY .= "Message: ".nl2br($_POST["message"]).""; 
+mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
+    echo ("<p class=\"response\">Thank you for contacting me!</br> Your message has been recieved and I'll get back with you as soon as possible.</br> Have a wonderful day!</p>");
+     
 ?>
+
+				</div>
+						</div>
+
+					</body>
+
+					<footer>
+						<div id="footer">
+						  
+					            <p class="copyright">Designed and developed by &copy; Brittany Howes 2014. All Rights Reserved.</p>
+					    </div>
+				             
+				    </footer>
+
+
+					</html>
+
